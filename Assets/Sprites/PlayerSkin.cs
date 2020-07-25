e09a0a349ebffed52fc688069dc00c3afa2fdc7f
Maks <maksterekhov1704@gmail.com>
2020-07-25 14:08:40 +0300

    Initial commit

---
+++ Assets/Sprites/PlayerSkin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New player skin", menuName = "Skins")]
+public class PlayerSkin : ScriptableObject
+{
+    public string SkinName;
+    public Sprite squareSrtite;
+    public Sprite roundSprite;
+    public Animation winAnimation;
+
+    public override int GetHashCode()
+    {
+        return (SkinName, squareSrtite, roundSprite).GetHashCode();
+    }
+    public override bool Equals(object other)
+    {
+        if(other is PlayerSkin player)
+        return this == player;
+        else
+        return false;
+    }
+    public static bool operator ==(PlayerSkin first, PlayerSkin second)
+    {
+        return first?.SkinName == second?.SkinName;
+    }
+    public static bool operator !=(PlayerSkin first, PlayerSkin second)
+    {
+        return !(first == second);
+    }
+}
